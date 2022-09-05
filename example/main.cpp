818be6c9d4770398d4438ccac44707de517032a3
nudistkiwi <58535135+nudistkiwi@users.noreply.github.com>
2022-09-05 14:50:31 +0200

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,1 +1,17 @@
+#include "https_server_basic.h"
 
+
+
+std::string callback(std::string A){
+  
+ std::string out="test.json";
+ return(out);};
+  
+  
+int main(){
+  
+   std::function<std::string(std::string)> func=callback;
+   start_http_server(func);
+  
+  
+}
